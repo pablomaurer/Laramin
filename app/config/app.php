a26fc3d6014ddf8a4e91fc04afadec97e9a3a7a7
mnewmedia <maurer@screeneyes.com>
2014-05-30 12:08:37 +0200

    IDE AutoCompletion Helper installed

--- app/config/app.php
+++ app/config/app.php
@@ -125,6 +125,7 @@ return array(
         'Lavary\Menu\ServiceProvider',
         'Pingpong\Widget\WidgetServiceProvider',
         'Mpedrera\Themify\ThemifyServiceProvider',
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
 	),
 
 	/*
