df46de3bb174e05302aa46dd2a5dbe5859a75334
mnewmedia <maurer@screeneyes.com>
2014-07-30 16:46:45 +0200

    master blade is now cleaner

--- app/themes/default/layouts/master.blade.php
+++ app/themes/default/layouts/master.blade.php
@@ -3,16 +3,21 @@
 
 <head>
     @section('head')
+    <!-- Bootstrap, Bootstrap validator styles -->
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.0/css/bootstrapValidator.min.css"/>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
+    <!-- jQuery, Bootstrap, Bootstrap validator scripts -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.0/js/bootstrapValidator.min.js"></script>
+
+    <!-- Locale translation -->
     @if (App::getLocale() == 'de')
         {{ HTML::script('lib/js/bootstrapValidator-de_DE.js') }}
     @endif
 
+    <!-- Custom Script and Styles -->
     {{ HTML::script('lib/js/master.js') }}
     {{ HTML::style('lib/css/style.css') }}
     @show
